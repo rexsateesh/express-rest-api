02947bdc22654ea5a515ab148873eaf92b3da607
rexsateesh <rex.sateesh@gmail.com>
2020-12-02 09:56:56 +0530

    removed ebs and corrected ip

--- routes/routes.js
+++ routes/routes.js
@@ -3,7 +3,7 @@ const appRouter = (app) => {
     res.status(200).send("Welcome to our restful API");
   });
 
-  app.get("/ip", (req, res) => res.send(req));
+  app.get("/ip", (req, res) => res.json({ipv4: req.ip, ipv6: req.connection.remoteAddress || 'nul'}));
 }
 
 module.exports = appRouter;
