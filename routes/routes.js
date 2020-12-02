167adf098b7306db972d2825a2be4a9bc1788daa
rexsateesh <rex.sateesh@gmail.com>
2020-12-02 08:37:57 +0530

    elasticbeanstalk config added

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,9 @@
-var appRouter = function (app) {
-  app.get("/", function(req, res) {
+const appRouter = (app) => {
+  app.get("/", (req, res) => {
     res.status(200).send("Welcome to our restful API");
   });
+
+  app.get("/ip", (req, res) => res.send(req));
 }
 
 module.exports = appRouter;
