5e96ae43ecc96269d59f313559f3afefd88bcb53
rexsateesh <rex.sateesh@gmail.com>
2020-12-14 16:01:36 +0530

    listening on both ipv4 and ipv6

--- app.js
+++ app.js
@@ -5,10 +5,43 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const getOsIps = () => {
+    const os = require('os');
+    let networkInterfaces = os.networkInterfaces();
+    let nonLocalInterfaces = {};
+    for (let inet in networkInterfaces) {
+        let addresses = networkInterfaces[inet];
+        for (let i=0; i<addresses.length; i++) {
+            let address = addresses[i];
+            if (!address.internal) {
+                if (!nonLocalInterfaces[inet]) {
+                    nonLocalInterfaces[inet] = [];
+                }
+                nonLocalInterfaces[inet].push(address);
+            }
+        }
+    }
+
+    return nonLocalInterfaces;
+}
+
+const getLocalIpv6 = () => {
+    const netBlocks = getOsIps();
+    for(let blocks of Object.keys(netBlocks)) {
+        for(let block of netBlocks[blocks]) {
+            if (block.family === 'IPv6') {
+                return block.address;
+            }
+        }
+    }
+}
+
 app.get("/", (req, res) => res.json({
+    ip: req.ip,
     ipForwarded: req.header('X-Forwarded-For') || null,
     remoteAddress: req.connection.remoteAddress,
     socketRemoteAds: req.socket.remoteAddress,
+    localIp: getLocalIpv6()
 }));
 
 const listenServer = () => {
@@ -16,7 +49,7 @@ const listenServer = () => {
 };
 
 // Use IPv4 address to listen on IPv4
-app.listen(8080, listenServer);
+app.listen(8080, '0.0.0.0', listenServer);
 
 // Use IPv6 address to listen on IPv6.
-// app.listen(8080, '2401:4900:169e:80c3:11e9:63ba:1f81:d224', listenServer);
+app.listen(8080, getLocalIpv6(), listenServer);
