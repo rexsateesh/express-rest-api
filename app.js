7c8ca2f9a91e1cf34205d2ce1b4143973e22752b
rexsateesh <rex.sateesh@gmail.com>
2021-08-17 15:11:30 +0530

    Removed unused code and vpc config

--- app.js
+++ app.js
@@ -1,53 +1,18 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-const getOsIps = () => {
-    const os = require('os');
-    let networkInterfaces = os.networkInterfaces();
-    let nonLocalInterfaces = {};
-    for (let inet in networkInterfaces) {
-        let addresses = networkInterfaces[inet];
-        for (let i=0; i<addresses.length; i++) {
-            let address = addresses[i];
-            if (!address.internal) {
-                if (!nonLocalInterfaces[inet]) {
-                    nonLocalInterfaces[inet] = [];
-                }
-                nonLocalInterfaces[inet].push(address);
-            }
-        }
-    }
-
-    return nonLocalInterfaces;
-}
-
-const getLocalIpv6 = () => {
-    const netBlocks = getOsIps();
-    for(let blocks of Object.keys(netBlocks)) {
-        for(let block of netBlocks[blocks]) {
-            if (block.family === 'IPv6') {
-                return block.address;
-            }
-        }
-    }
-}
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => res.json({
     ip: req.ip,
     ipForwarded: req.header('X-Forwarded-For') || null,
     remoteAddress: req.connection.remoteAddress,
     socketRemoteAds: req.socket.remoteAddress,
-    // localIp: getLocalIpv6()
 }));
 
-const listenServer = () => {
-    console.log('App running on');
-};
-
 // Listen server
-app.listen(8080, listenServer);
+app.listen(8080, () => {
+    console.log('App running on');
+});
 
